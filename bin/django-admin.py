e732ea7cc13526867f227eceadd898fa0208eff5
James Kafader <jkafader@aimmedia.com>
2014-04-01 15:08:38 -0700

    Initial commit.

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jkafader/Documents/IA_Workspace/archive-it/wireframe-django/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
